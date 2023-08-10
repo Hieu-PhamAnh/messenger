aa38c11123462b80c91940db7eb9bec6b89faad4
Phạm Anh Hiếu <HIEUPA.B19CN254@stu.ptit.edu.vn>
2023-08-10 14:31:21 +0700

    spawn token

--- messenger/src/auth/auth.service.ts
+++ messenger/src/auth/auth.service.ts
@@ -7,10 +7,16 @@ import {
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto, CreateUserDto } from './dto';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signUp(dto: CreateUserDto) {
     // console.log(dto);
@@ -31,7 +37,9 @@ export class AuthService {
     }
   }
 
-  async signIn(dto: AuthDto) {
+  async signIn(
+    dto: AuthDto,
+  ): Promise<{ access_token: string; refresh_token: string }> {
     console.log(dto);
     try {
       const user = await this.prisma.user.findFirst({
@@ -45,9 +53,32 @@ export class AuthService {
       if (dto.password !== user.password) {
         throw new UnauthorizedException('wrong password');
       }
-      return user;
+      const access_token = await this.spawnAccessToken(user.id, user.userName);
+      const refresh_token = await this.spawnRefreshToken(
+        user.id,
+        user.userName,
+      );
+      return { access_token, refresh_token };
     } catch (error) {
       throw error;
     }
+  }
+
+  async spawnAccessToken(id: number, username: string): Promise<string> {
+    const payload = { sub: id, username };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: this.config.get('ACCESS_EXP'),
+      secret: this.config.get('ACCESS_KEY'),
+    });
+    return token;
+  }
+
+  async spawnRefreshToken(id: number, username: string): Promise<string> {
+    const payload = { sub: id, username };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: this.config.get('REFRESH_EXP'),
+      secret: this.config.get('REFRESH_KEY'),
+    });
+    return token;
   }
 }
